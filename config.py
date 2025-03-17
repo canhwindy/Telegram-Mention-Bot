b9dba42794412b63cad52a11a4bbdc241fbb13b1
canhwindy <132927910+canhwindy@users.noreply.github.com>
2025-03-17 10:46:20 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Telegram API credentials
+API_ID = xx
+API_HASH = 'xxx'
+BOT_TOKEN = 'xxxxx'
+
+# List of group IDs where any member can use the command
+UNRESTRICTED_GROUPS = [-1001671742315]  # Replace with your actual group IDs
+
+# File paths for videos
+RUDE_MESSAGE_VIDEO = r'C:\\Users\\LENOVO\\Downloads\\videoplayback.mp4'
+SPECIFIC_MESSAGE_VIDEO = r'C:\\Users\\LENOVO\\Pictures\\gif\\creepy-doraemon-by-error.mp4'
